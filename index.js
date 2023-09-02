cfb3c7c16e8bc7512bf9af1888d0219b1a52f57b
Daniel Busis <busis.daniel@gmail.com>
2023-09-02 13:25:37 -0400

    Use nanotimer for server physics

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const _ = require("lodash")
+const NanoTimer = require('nanotimer');
+const timer = new NanoTimer();
 
 /**
  * TODO:
@@ -22,6 +24,7 @@ const _ = require("lodash")
  *    - Use for server-predicted-position
  *  - helper fxns for interpolation on client side
  *  - enable TS
+ *  - Switch server-side physics to use `while(oldTime + frameTime < newTime){...}`
  *  - maybe:
  *    - Keep a buffer of player inputs, received once per frame
  *      - Apply them once per server-tick, and tell the client the ID of the latest one processed
@@ -74,18 +77,13 @@ http.listen(port, () =>
   console.log(`Multiplayer app listening on port: ${port}`)
 );
 
-var serverLoopVar;
-function serverLoop() {
-  io.emit("updatePos", {playerPositions, time: Date.now()});
-  serverLoopVar = setTimeout(serverLoop, 45);
-}
+// Server-side physics loop and update messages
 
-var serverPhysicsLoopVar;
-function serverPhysicsLoop(prevTime) {
-  const curTime = Date.now();
-  const deltaTime = curTime - prevTime;
-  // console.log(deltaTime);
+const SERVER_TICK_TIME_MICRO = Math.round(1000 * 1000 / 60);
+let serverTime = performance.now();
+let serverFrameNumber = 0;
 
+const doPhysics = (deltaTime) => {
   for (entry of Object.entries(playerInputs)) {
     if (entry[0] > 0 && playerPositions[entry[0]]) {
       if (entry[1].up) playerPositions[entry[0]].y -= PLAYER_SPEED_PER_MS * deltaTime;
@@ -94,11 +92,23 @@ function serverPhysicsLoop(prevTime) {
       if (entry[1].left) playerPositions[entry[0]].x -= PLAYER_SPEED_PER_MS * deltaTime; 
     }
   }
-
-  serverPhysicsLoopVar = setTimeout(() => serverPhysicsLoop(curTime), 15);
 }
 
-serverLoop();
-serverPhysicsLoop(Date.now());
+const sendPositionUpdates = () => {
+  io.emit("updatePos", {playerPositions, time: Date.now()});
+}
 
+const serverTick = () => {
+  const newServerTime = performance.now();
+  serverFrameNumber++;
+
+  doPhysics(newServerTime - serverTime);
+  if (serverFrameNumber % 3 === 0) {
+    sendPositionUpdates();
+  }
+
+  serverTime = newServerTime;
+}
+
+timer.setInterval(serverTick, '', `${SERVER_TICK_TIME_MICRO}u`);
 
